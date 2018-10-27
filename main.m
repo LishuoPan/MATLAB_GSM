b46d197f8f040fe023d129f003786911367db1c5
pls19961219@gmail.com <pls19961219@gmail.com>
2018-10-27 17:53:42 +0800

    revise to main.m

--- main.m
+++ main.m
@@ -19,37 +19,42 @@ options_gen = struct('freq_lb', 0, 'freq_ub', 0.5, ...
                  'fix_var', 0.001, 'sampling', 0 );
 
 [freq, var, Q] = generateGSM(options_gen); % the length of freq or var is Q we need
-K = kernelComponent(freq, var, xtrain, xtrain);
 
-% Kernel matrix low rank approximation
-
-
-% Hyperpara Opt
-
-% ADMM ML Opt
-% options_ADMM = struct('rho', 2000, 'MAX_iter', 50, 'nv', varEst, ...
-%                       'iniAlpha', 200*ones(Q,1));
-% 
-% alpha = ADMM_ML(ytrain,K,options_ADMM);
-
-
-% DCP Opt
-
-L = cell(1,Q);
-for kk =1:Q
-L{kk} = (cholcov(K{kk})).';
+Nystrom_activate = 0;
+if Nystrom_activate == 0
+    K = kernelComponent(freq, var, xtrain, xtrain);
+    L = cell(1,Q);
+    for kk =1:Q
+        L{kk} = (cholcov(K{kk})).';
+    end
+else
+    % Kernel matrix low rank approximation
+    nys_sample = ceil(length(xtrain)/20);
+    [L,K] = Nystrom(xtrain,freq,var,nys_sample);
 end
 
-Phi = eye(nTrain);
-iniAlpha = ini_Alpha('fix', 0, Q, ytrain, K);
-options_DCP = struct('verbose',1,'ev',false, ...
-                 'nv',varEst, ...
-                 'dimension_reduction',true, ...
-                 'c_nv',0.0, ...
-                 'c_alpha', iniAlpha,...
-                 'maxiters', 30);
-[alpha,nv,info] = mkrm_optimize(ytrain,Phi,L,options_DCP);
+% Hyperpara Opt
+Opt_method = 0;
 
+if Opt_method == 1
+    % ADMM ML Opt
+    options_ADMM = struct('rho', 2000, 'MAX_iter', 50, 'nv', varEst, ...
+                          'iniAlpha', 200*ones(Q,1));
+
+    alpha = ADMM_ML(ytrain,K,options_ADMM);
+    
+elseif Opt_method == 0
+    % DCP Opt
+    Phi = eye(nTrain);
+    iniAlpha = ini_Alpha('fix', 0, Q, ytrain, K);
+    options_DCP = struct('verbose',1,'ev',false, ...
+                     'nv',varEst, ...
+                     'dimension_reduction',true, ...
+                     'c_nv',0.0, ...
+                     'c_alpha', iniAlpha,...
+                     'maxiters', 30);
+    [alpha,nv,info] = mkrm_optimize(ytrain,Phi,L,options_DCP);
+end
 
 % prediction (test phase)
 [pMean, pVar] = prediction(xtrain,xtest,ytrain,nTest,alpha,varEst,freq,var,K);
