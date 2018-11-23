a6bfbac37bf74eb6ba5f7726e96b54f5d8777c56
pls19961219@gmail.com <pls19961219@gmail.com>
2018-11-23 17:27:26 +0800

    plot: remove shadow

--- functions/plot_save.m
+++ functions/plot_save.m
@@ -1,8 +1,8 @@
 function [] = plot_save(xtrain,ytrain,xtest,ytest,nTest,pMean_DCP,pVar_DCP,pMean_ADMM,figName)
 figure(); hold on;
 plot(xtrain,ytrain,'b','LineWidth',2);
-f = [pMean_DCP(1:nTest) + 2*sqrt(pVar_DCP(1:nTest)); flip(pMean_DCP(1:nTest) - 2*sqrt(pVar_DCP(1:nTest)),1)];
-fill([xtest(1:nTest); flip(xtest(1:nTest),1)], f, [7 7 7]/8, 'EdgeColor', [7 7 7]/8);
+% f = [pMean_DCP(1:nTest) + 2*sqrt(pVar_DCP(1:nTest)); flip(pMean_DCP(1:nTest) - 2*sqrt(pVar_DCP(1:nTest)),1)];
+% fill([xtest(1:nTest); flip(xtest(1:nTest),1)], f, [7 7 7]/8, 'EdgeColor', [7 7 7]/8);
 p1 = plot(xtest(1:nTest), ytest(1:nTest), 'g','LineWidth',2); 
 p2 = plot(xtest(1:nTest), pMean_DCP(1:nTest),'b-','LineWidth',1.5);
 
