812d43e936ce3a2623f58a38a665c3385d541a6c
pls19961219@gmail.com <pls19961219@gmail.com>
2018-12-02 13:52:21 +0800

    Auto Gradient selection

--- functions/S_gradient.m
+++ functions/S_gradient.m
@@ -10,25 +10,25 @@ function S_g = S_gradient(ytrain, S, L, C, rho, method)
     n = length(ytrain);
     eye_M = eye(n);
     rank_one = ytrain * ytrain';
-    
-    if method == 0
+    indicator = norm(S*C-eye_M,'fro');
+    if indicator > 0.1
         psi = S*C;
         zeta = -rho*eye_M + L + rho*psi;
         eta = zeta*C;
         S_g = 2*rank_one - rank_one.*eye_M ...
               - 2*inv(S) + inv(S).*eye_M ...
               + eta + eta' - eta.*eye_M;
-    elseif method == 1
+    elseif indicator <= 0.1
         psi = S*C;
         phi = (-1-rho)*eye_M + L + rho*psi;
         omega = phi*C;
         S_g = 2*rank_one - rank_one.*eye_M ...
               + omega + omega' - omega.*eye_M;
-    else
-        I_L = -eye_M + L;
-        I_L_C = I_L * C;
-        S_g = 2*rank_one - rank_one.*eye_M ...
-              + I_L_C + I_L_C' - I_L_C.*eye_M;
+%     else
+%         I_L = -eye_M + L;
+%         I_L_C = I_L * C;
+%         S_g = 2*rank_one - rank_one.*eye_M ...
+%               + I_L_C + I_L_C' - I_L_C.*eye_M;
     end
     
 end
