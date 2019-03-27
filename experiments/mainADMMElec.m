38db9f9aa70d0698bb521311d23c6bbe3e8b24a2
pls19961219@gmail.com <pls19961219@gmail.com>
2019-03-27 17:08:36 +0800

    notation and ADMM_ML function name rename

--- experiments/mainADMMElec.m
+++ experiments/mainADMMElec.m
@@ -1,11 +1,11 @@
 clc;clear;
-addpath('./functions')
-addpath('./functions/l1_ls_matlab')
-addpath('./data')
+addpath('../functions')
+addpath('../functions/l1_ls_matlab')
+addpath('../data')
 addpath ~/mosek/8/toolbox/r2014a
 
 % Read in data & some general setup
-file_name = 'unemployment';
+file_name = 'electricitydata';
 disp(['Simulation on ',file_name]);
 [xtrain, ytrain, xtest, ytest] = load_data(file_name);
 nTrain = length(xtrain);
@@ -40,43 +40,29 @@ end
 
 % Hyperpara Opt
 
-% 1-step DCP Opt
-Phi = eye(nTrain);
-iniAlpha = ini_Alpha('fix', 0, Q, ytrain, K);
-% DCP settings
-options_DCP = struct('verbose',1,'ev',false, ...
-                 'nv',varEst, ...
-                 'dimension_reduction',true, ...
-                 'c_nv',0.0, ...
-                 'c_alpha', iniAlpha,...
-                 'maxiters', 1);
-% DCP step
-[alpha_DCP,nv,info] = mkrm_optimize(ytrain,Phi,L,options_DCP);
-[pMean_DCP, pVar_DCP] = prediction(xtrain,xtest,ytrain,nTest,alpha_DCP,varEst,freq,var,K);
-
+% IniAlpha obtained from Periodogram Method Estimation
+[iniAlpha_Pdg, goodness] = alphaIniFromPeriodogram(ytrain, Q, freq, var(1));
 
 % ADMM settings
 % MaxIL: numbers of Internal iteraions in gradient method; 
 % MAX_iter: numbers of total outer iterations.
-options_ADMM = struct('rho', 100, 'rho_dual', 50, 'MaxIL', 1000, 'mu', 1e-6, 'MAX_iter', 30000, 'nv', varEst, ...
-                      'iniAlpha', alpha_DCP);
+options_ADMM = struct('rho', 100, 'rho_dual', 50, 'MaxIL', 1000, 'mu', 1e-6, 'MAX_iter', 3000, 'nv', varEst, ...
+                      'iniAlpha', iniAlpha_Pdg);
 % ADMM step
-[alpha, AugObjEval, OriObjEval, Gap] = ADMM_ML_plot(xtrain,xtest,ytrain,ytest,nTest,varEst,freq,var,K,options_ADMM);
-% Plot the convergence criteria
+[alpha, AugObjEval, OriObjEval, Gap] = ADMM_ML(xtrain,xtest,ytrain,ytest,nTest,varEst,freq,var,K,options_ADMM);
+% Plots of convergence criteria
 figure;plot(AugObjEval);title('Iterations v.s. Augmanted Objective');xlabel('iterations');ylabel('Aug Obj');
 figure;plot(OriObjEval);title('Iterations v.s. Original Objective');xlabel('iterations');ylabel('Original Obj');
 figure;plot(Gap);title('Iterations v.s. Gap');xlabel('iterations');ylabel('Gap');
 figure;bar(alpha);title('alpha after ADMM');xlabel('index');ylabel('alpha value');
 
 
-% Prediction
-[pMean_final, pVar_final] = prediction(xtrain,xtest,ytrain,nTest,alpha,varEst,freq,var,K);
-MSE_final = mean((pMean_final-ytest(1:nTest)).^2);
-
-
-% Plot phase
+% Prediction and plot phase
+[pMean, pVar] = prediction(xtrain,xtest,ytrain,nTest,alpha,varEst,freq,var,K);
+MSE = mean((pMean-ytest(1:nTest)).^2);
 figName = ['./fig/Temp',file_name,'Q',int2str(Q)];
-plot_save_compare(xtrain,ytrain,xtest,ytest,nTest,pMean_DCP,pVar_DCP,pMean_final,figName,file_name);
+plot_save(xtrain,ytrain,xtest,ytest,nTest,pMean,pVar,figName,file_name);
+
 
 
 
